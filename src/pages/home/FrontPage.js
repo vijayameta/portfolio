e3c167bed26e07b7ea475c3cb893c17de4a9d950
vijayameta <112999776+vijayameta@users.noreply.github.com>
2024-11-07 21:09:11 +0530

    resume url

--- src/pages/home/FrontPage.js
+++ src/pages/home/FrontPage.js
@@ -33,7 +33,7 @@ const navItems = [
 ];
 
 const handleOnClick = () => {
-    const url = "https://drive.google.com/file/d/1nBzPvfHMxI7EKtJ7LjocdgoyXoaI_RSM/view?usp=drive_link";
+    const url = "https://drive.google.com/file/d/1PAGhYty8ZFq-YvaAWKNB1KN4Kr8OfETN/view?usp=drive_link";
     window.open(url, "_blank");
 };
 
