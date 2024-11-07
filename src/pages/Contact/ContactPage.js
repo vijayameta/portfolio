41781ab5ec358ee92b3cbaccbe15fb43add27dad
vijayameta <112999776+vijayameta@users.noreply.github.com>
2024-11-07 19:58:33 +0530

    updated url

--- src/pages/Contact/ContactPage.js
+++ src/pages/Contact/ContactPage.js
@@ -86,7 +86,7 @@ function DrawerAppBar(props) {
   const handleSendMessage = async () => {
     if (validateForm()) {
       try {
-        const response = await axios.post('https://portfolio-backend-2qtiwy6ix-vijay-ametas-projects.vercel.app/createdata', formData);
+        const response = await axios.post('https://portfolio-seven-pi-10.vercel.app/createdata', formData);
         console.log(response.data);
         setIsFormSubmitted(true);
       } catch (error) {
